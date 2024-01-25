5cb6b93cfc0c85d37c2fe3f80d0c18cdd7bb1b80
Mohd Gulrez <115737338+gulrezpasha@users.noreply.github.com>
2024-01-25 13:23:43 +0000

    variables

---
+++ .devcontainer/01_basics/variable.js
@@ -0,0 +1,14 @@
+const accountId=12345
+let accountEmail="pasha@gmail.com"
+var accountPassword="45632"
+accountCity="Moradabad"
+
+accountEmail="Turk@gmail.com"
+accountPassword="67543"
+accountCity="Chandigarh"
+
+// const can't be change but let can be change and not to use var because of 
+// issue in block and functional scope
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity]);
