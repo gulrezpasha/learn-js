be6b81db6128d14da2e4a7ba0a32d96f431438bf
Mohd Gulrez <115737338+gulrezpasha@users.noreply.github.com>
2024-02-06 10:37:29 +0000

    pasha

---
+++ .devcontainer/01_basics/datatypes.js
@@ -0,0 +1,15 @@
+const id=Symbol('123');
+
+const anotherId=Symbol('123');
+
+console.log(id===anotherId);
+
+const star=["pasha","Mughal"];
+let myObj={
+    name: "turk",
+    age: 21,
+}
+const myFunction = function(){ // here we declate function as a variable 
+    console.log("less go");
+}
+console.log(typeof id);
