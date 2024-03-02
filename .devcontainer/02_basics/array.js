4a2cd7d2c2eb3cf1cb54ebf9862cbbdafaac44da
Mohd Gulrez <115737338+gulrezpasha@users.noreply.github.com>
2024-03-02 08:54:47 +0000

    devcontainer

--- .devcontainer/02_basics/array.js
+++ .devcontainer/02_basics/array.js
@@ -1,20 +1,32 @@
-const myArr=[0,3,4,5,6]
-console.log(myArr[1]);
+// const myArr=[0,3,4,5,6]
+// console.log(myArr[1]);
 
 // myArr.push(5)
 // myArr.pop();
 
 // myArr.unshift(3) add at starting
-const newArr=myArr.join();// convert array into string
-console.log(myArr);
-console.log(newArr);
+//const newArr=myArr.join();// convert array into string
+// console.log(myArr);
+// console.log(newArr);
 
-console.log("A",myArr);
+// console.log("A",myArr);
 
-const myn1=myArr.slice(2,4);
-console.log(myn1);
+// const myn1=myArr.slice(2,4);
+// console.log(myn1);
 
-console.log("B",myArr);
-const myn2=myArr.splice(2,4);
-console.log("C",myArr);
-console.log(myn2);
+// console.log("B",myArr);
+// const myn2=myArr.splice(2,4);
+// console.log("C",myArr);
+// console.log(myn2);
+
+const heros=['p','q','r']
+const dhero=['w','r','t']
+
+const allnew=heros.concat(dhero);
+const alln=[...heros, ...dhero]
+
+console.log(allnew);
+console.log(alln);
+
+
+
