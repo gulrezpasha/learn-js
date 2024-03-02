f32ac78a4718a67780cad3e65df042d2ec526ac6
Mohd Gulrez <115737338+gulrezpasha@users.noreply.github.com>
2024-03-02 14:15:20 +0000

    container config done

---
+++ .devcontainer/02_basics/02_object.js
@@ -0,0 +1,35 @@
+const tinderuser={}
+ tinderuser.id="11318"
+ tinderuser.name="pasha"
+ tinderuser.isLoggedIn=false;
+
+//  console.log(tinderuser);
+const regularuser={
+    email:"wer@gmail.com",
+    fullname:{
+        userfullname:{
+            firstname:"gulrez",
+            lastname:"pasha"
+        }
+    }
+}
+// console.log(regularuser.fullname);
+
+const obj1={1:"a",2:"b"}
+const obj2={3:"a",4:"b"}
+
+// const obj3=Object.assign(obj1,obj2);
+const obj3={...obj1,...obj2} //spread mthd 
+console.log(obj3);
+
+const users=[
+    {
+        id:1,
+        email:"h@gmail.com"
+    }
+]
+// users[1].email;
+console.log(tinderuser);
+console.log(Object.keys(tinderuser));
+console.log(Object.values(tinderuser));
+console.log(Object.entries (tinderuser));
