06c2867ecfd94b5b910e271f6e5971f900a7cf3d
Mohd Gulrez <115737338+gulrezpasha@users.noreply.github.com>
2024-03-04 13:25:41 +0000

    control flow files

---
+++ .devcontainer/03_basics/arrow.js
@@ -0,0 +1,23 @@
+const user={
+    username: "pasha",
+    price:999,
+
+    welcomemessage:function(){
+        console.log(`${this.username},welcome to message`);
+    }
+}
+user.welcomemessage();
+
+// const val=(a1,a2)=>  a1+a2
+const val=(a1,a2)=> ( a1+a2)
+
+
+console.log(val(2,5))
+
+// iffi when global scope polluted our cirrent inner scope or function
+
+// Immediately Invoked Function Expressions (IIFE)
+
+
+
+
---
+++ .devcontainer/03_basics/iffi.js
@@ -0,0 +1,12 @@
+(function chai(){
+    // named IIFE
+    console.log(`DB CONNECTED`);
+})();
+
+( (name) => {
+    console.log(`DB CONNECTED TWO ${name}`);
+} )('HANEY');
+
+(()=>{
+    console.log(`pASHA`);
+})()
