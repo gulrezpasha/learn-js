0bd3291c49b7f1ed19d588d2f65351be1a839b99
Mohd Gulrez <115737338+gulrezpasha@users.noreply.github.com>
2024-03-05 15:47:43 +0000

    array loop

---
+++ .devcontainer/05_iteration/one.js
@@ -0,0 +1,11 @@
+let array=[2,3,4,5,6];
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    console.log(element);
+}
+
+let index=0;
+while(index <=10){
+    console.log(`value of index ${index}`);
+    index=index+2;
+}
